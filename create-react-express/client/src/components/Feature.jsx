5664b8d85a7734f6dd5a09d46ad32548f33ccb38
Ayemae <AlyssaAlecci@gamil.com>
2018-08-20 13:47:30 -0400

    shiny css

---
+++ create-react-express/client/src/components/Feature.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Feature extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            <img src={props.thumbnail}/>
+            </div>
+        )
+    }
+}
+
+export default Feature;
