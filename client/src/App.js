bc9a4c929de03392a1e5930f25d7d7de259f5580
Alyssa <AlyssaAlecci@gmail.com>
2018-08-27 13:29:04 -0400

    css

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import "./App.css";
 import Form from "./components/Form/FormLogic";
 import Display from "./components/Display/Display";
 import MainPage from "./components/MainPage/MainPage";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 
 
 const App = () => (
-  
-      <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/display" component={Display} />
-          <Route exact path="/form" component={Form} />
-          </Switch>
-      </div>
-      </Router>
-  
+
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/display" component={Display} />
+        <Route exact path="/form" component={Form} />
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
+
 );
 
 export default App;
